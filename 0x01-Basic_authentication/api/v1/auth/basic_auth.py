9aa0999cc91eddf556457cd56be57e27c949d09f
sirln <siro@sirlawren.com>
2024-02-29 20:24:48 +0300

    Basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Basic Authentication Module
+'''
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    '''
+    Basic Authentication Class
+    Inheriting from Auth Class to implement
+    basic authentication.
+    '''
+    pass
